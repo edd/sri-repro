e57a43aca6bbeb5c6e99ff2f52c99541c1d42b3c
Edd Hannay <edd@vega.xyz>
2022-06-28 10:53:21 +0100

    initial commit

---
+++ apps/sri-repro-app/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="sri-repro-app" />
+      <div />
+    </>
+  );
+}
+
+export default App;
